1d4c5ce224eece8cce8f775ec04334f505e9bf5f
Jorge Torres <jorge.albertorres@gmail.com>
2022-11-26 22:46:29 -0600

    Create commands DML.sql

---
+++ commands DML.sql
@@ -0,0 +1,67 @@
+--
+--SELECT
+--INSERT
+--UPDATE
+--DELATE
+
+-- FROM
+-- WHERE
+-- GROUP BY
+-- HAVING
+-- ORDER BY
+
+LOGICAL OPERATORS
+-- AND
+-- OR
+--NOT
+
+-- <
+-- >
+-- <>
+-- <=
+-- >=
+-- =
+-- BETWEEN
+-- LIKE
+-- IN
+
+-- AVG
+-- COUNT
+-- SUM
+-- MAX
+-- MIN
+
+
+--
+--SELECT
+SELECT * FROM productos;
+--INSERT
+--UPDATE
+--DELATE
+
+-- FROM
+-- WHERE
+-- GROUP BY
+-- HAVING
+-- ORDER BY
+
+LOGICAL OPERATORS
+-- AND
+-- OR
+--NOT
+
+-- <
+-- >
+-- <>
+-- <=
+-- >=
+-- =
+-- BETWEEN
+-- LIKE
+-- IN
+
+-- AVG
+-- COUNT
+-- SUM
+-- MAX
+-- MIN
