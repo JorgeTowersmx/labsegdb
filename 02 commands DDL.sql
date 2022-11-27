ef7940937ac8d0b9c6d726815aa695e0a244262b
Jorge Torres <jorge.albertorres@gmail.com>
2022-11-27 01:30:44 -0600

    Update 02 commands DDL.sql

--- 02 commands DDL.sql
+++ 02 commands DDL.sql
@@ -2,3 +2,13 @@
 --
 ALTER TABLE pedidos 
 RENAME COLUMN codigo_pedido TO codigo_producto;
+
+
+ALTER TABLE pedidos
+DROP codigo_pedidos;
+
+ALTER TABLE pedidos
+RENAME TO pedidos_nuevos;
+
+ALTER TABLE pedidos
+ALTER COLUMN notes TYPE varchar(500);
