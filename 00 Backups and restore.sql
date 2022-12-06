6b65dd948e4a1c13c2293a301c8d4bee406c9e94
Jorge Torres <jorge.albertorres@gmail.com>
2022-12-05 23:56:09 -0600

    Update 00 Backups and restore.sql

--- 00 Backups and restore.sql
+++ 00 Backups and restore.sql
@@ -5,3 +5,6 @@
 --Since terminal  example = root@debian#mysqldump 
 
 mysqldump -u root -p name_database > respaldo_name_database.sql
+
+--Restore
+mysql -u usuario -p name_database < respaldo_name_database.sql
