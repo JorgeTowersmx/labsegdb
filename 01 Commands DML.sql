f8bb8fc192393c35257bb99021df5669ab372f37
Jorge Torres <jorge.albertorres@gmail.com>
2022-12-07 03:48:59 -0600

    Update 01 Commands DML.sql

--- 01 Commands DML.sql
+++ 01 Commands DML.sql
@@ -5,6 +5,8 @@ SELECT domicilio FROM cliente WHERE nombre='Juan';
 --INSERT
 INSERT INTO pedidos (id_pedido, id_cliente, codigo_producto, fechaped, fechaenvio) values (74, 74, 74, '5/31/2021', '6/16/2021');
 INSERT INTO cliente VALUES (1,'Jorge','Lopez','Martinez','Av Siempre Viva #1000');
+INSERT INTO users(name, email, country, cp) VALUES('Jorge',
+                                                   'jsistemas@fake.com','MXN','00018');
 --UPDATE
 --DELETE
 
