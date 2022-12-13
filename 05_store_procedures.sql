def37c621263578f787ce66ba5a8f5a249837c1a
Jorge Torres <jorge.albertorres@gmail.com>
2022-12-13 04:23:39 -0600

    Create 05_store_procedures.sql
    Signed-off-by: Jorge Torres <jorge.albertorres@gmail.com>

---
+++ 05_store_procedures.sql
@@ -0,0 +1,14 @@
+-- Command DDL TYPE
+-- SP Insert 3 values in table users
+DELIMITER //
+CREATE PROCEDURE insert_users(IN p_username VARCHAR(50), IN p_password VARCHAR(150), IN p_email VARCHAR(100))
+BEGIN 
+    INSERT INTO users(username,password,email) VALUES (p_username,p_password,p_email);
+END //
+DELIMITER ;
+
+--USE SP insert_users
+CALL insert_users('usuariox','paswordy','correo@fake.com');
+
+-- DROP SP 
+DROP PROCEDURE insert_users;
